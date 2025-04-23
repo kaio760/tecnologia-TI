fe9115904706f67ac02a94e45c39775042ce90c7
kaio760 <kaiolimacampos517@gmail.com>
2025-04-23 09:31:39 -0300

    .

---
+++ PHP/estrutura basica/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura básica PHP</title>
+</head>
+<body>
+
+<?php
+$str='Meu primeiro arquivo php';
+echo $str;
+?>
+    
+</body>
+</html>
